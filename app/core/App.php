67310c726953290e607853fd53ac0690dced48f8
BonariSimanjuntak <bonariiii96@gmail.com>
2023-01-03 15:56:14 +0700

    Membuat kerangka MVC native PHP

---
+++ app/core/App.php
@@ -0,0 +1,19 @@
+<?php
+
+class App {          
+    public function __construct()
+    {
+        $url=$this->ParseUrl();
+        var_dump($url);
+    }
+
+    public function ParseUrl()
+    {
+        if (isset($_GET['url'])) {  
+            $url=rtrim( $_GET['url']);
+            $url=filter_var($url, FILTER_SANITIZE_URL);
+            $url=explode('/',$url);
+            return $url;
+        }
+    }
+}
---
+++ app/core/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+class Controller {
+    public function __construct()
+    {
+        echo var_dump($_GET);
+    }
+}
